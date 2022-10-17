546d3beef9524f10453aebe7ab2a815a748ac3b6
Ashish Sarawad <113109562+AshishSarawad@users.noreply.github.com>
2022-10-17 10:52:28 +0530

    Update index.php
    <?php echo '<p>Hi Minfy Devops Team</p>'; ?> 
     <?php echo '<p>Welcome to PHPApp running inside Docker container!</p>'; ?> 
     <?php echo '<p>It is deployed Through jenkins pipeline</p>'; ?> ?

--- src/index.php
+++ src/index.php
@@ -26,4 +26,7 @@
 </tr>
 </table>
 </body>
+ <?php echo '<p>Hi Minfy Devops Team</p>'; ?> 
+ <?php echo '<p>Welcome to PHPApp running inside Docker container!</p>'; ?> 
+ <?php echo '<p>It is deployed Through jenkins pipeline</p>'; ?> ?
 </html>
